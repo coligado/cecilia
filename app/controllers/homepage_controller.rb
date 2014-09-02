b462eae2ce60fc7e3ff3982679e055c80098d325
Lea Coligado <leacoligado@Leas-MacBook-Pro.local>
2014-09-01 22:05:49 -0700

    Added method for processing the name

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,4 +4,22 @@ class HomepageController < ApplicationController
 
   def about
   end
+
+  def italianize
+  	first_name = params[:name][:first_name]
+    first_name.upcase
+    if first_name[-1,1] == 'A'
+      # check if it's in the female YAML file
+    else
+      # check if it's in the 
+    end
+
+  	@last_name = params[:name][:last_name]
+    render :partial => "shared/italian_name"
+  end
+
+  private
+  # def comment_params(params)
+  #   return params.permit(:comment)
+  # end
 end
